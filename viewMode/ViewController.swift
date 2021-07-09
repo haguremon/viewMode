f04ccfa3c7635c3c3f170e86ee82b8168f0dbc48
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-09 19:03:11 +0900

    Initial Commit

---
+++ viewMode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewMode
+//
+//  Created by IwasakIYuta on 2021/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
